5ffb8d8a8e3f7b382388619ae3828e58f3b8418a
Malachi Burke <malachi.burke@gmail.com>
2018-07-07 20:42:37 -0700

    Wrestling with build system to combine pieces
    Unsurprisingly tricky, gluing together:
    
    * uGFX
    * uGFX driver helper
    * frab
    * ESP32

--- test/rtos/esp32/template/components/component.mk
+++ test/rtos/esp32/template/components/component.mk
@@ -1,1 +1,4 @@
-COMPONENT_SRCDIRS := uGFX/src uGFX-drivers/src framework-abstraction/src
+COMPONENT_ADD_INCLUDEDIRS := uGFX/uGFX
+COMPONENT_SRCDIRS := uGFX/uGFX/src uGFX-drivers/src framework-abstraction/src
+#COMPONENT_SRCDIRS += 
+COMPONENT_OBJS := uGFX/uGFX/src/gfx_mk.o
