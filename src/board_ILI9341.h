83050aae9416870b2c0bdae889ee5fc6914bdee0
Malachi Burke <malachi.burke@gmail.com>
2018-07-08 18:42:42 -0700

    Stubbing out ILI9341 more

--- src/board_ILI9341.h
+++ src/board_ILI9341.h
@@ -1,1 +1,74 @@
 #pragma once
+
+/*
+ * This file is subject to the terms of the GFX License. If a copy of
+ * the license was not distributed with this file, you can obtain one at:
+ *
+ *              http://ugfx.org/license.html
+ */
+
+#ifndef _GDISP_LLD_BOARD_H
+#define _GDISP_LLD_BOARD_H
+
+void init_ili9341_board(GDisplay *g);
+void post_init_ili9341_board(GDisplay *g);
+void setpin_ili9341_reset(GDisplay *g, bool_t state);
+void set_ili9341_backlight(GDisplay *g, uint8_t percent);
+void acquire_ili9341_bus(GDisplay *g);
+void release_ili9341_bus(GDisplay *g);
+void write_ili9341_index(GDisplay *g, uint16_t index);
+void write_ili9341_data(GDisplay *g, uint16_t data);
+void ili9341_setreadmode(GDisplay *g);
+void ili9341_setwritemode(GDisplay *g);
+uint16_t ili9341_read_data(GDisplay *g);
+
+static GFXINLINE void init_board(GDisplay *g) {
+	init_ili9341_board(g);
+}
+
+static GFXINLINE void post_init_board(GDisplay *g) {
+	post_init_ili9341_board(g);
+}
+
+static GFXINLINE void setpin_reset(GDisplay *g, bool_t state) {
+	(void) g;
+	(void) state;
+}
+
+static GFXINLINE void set_backlight(GDisplay *g, uint8_t percent) {
+	(void) g;
+	(void) percent;
+}
+
+static GFXINLINE void acquire_bus(GDisplay *g) {
+	(void) g;
+}
+
+static GFXINLINE void release_bus(GDisplay *g) {
+	(void) g;
+}
+
+static GFXINLINE void write_index(GDisplay *g, uint16_t index) {
+	(void) g;
+	(void) index;
+}
+
+static GFXINLINE void write_data(GDisplay *g, uint16_t data) {
+	(void) g;
+	(void) data;
+}
+
+static GFXINLINE void setreadmode(GDisplay *g) {
+	(void) g;
+}
+
+static GFXINLINE void setwritemode(GDisplay *g) {
+	(void) g;
+}
+
+static GFXINLINE uint16_t read_data(GDisplay *g) {
+	(void) g;
+	return 0;
+}
+
+#endif /* _GDISP_LLD_BOARD_H */
--- src/gdisp/spi/ILI9341.hpp
+++ src/gdisp/spi/ILI9341.hpp
@@ -1,1 +1,43 @@
 #include <frab/spi.h>
+
+// some clues how we should do this here https://github.com/fcayci/stm32f1-uGFX but quite low level
+// more clues https://community.ugfx.io/topic/156-ili9341-spi-stm32f103/
+// more clues https://community.ugfx.io/topic/1000-generic-board-file-for-ili9341-and-the-arduino-ide-based-on-adadruits-generic-library-supporting-different-boards/
+
+extern "C" {
+
+#include <gfx.h>
+#include <string.h>
+
+}
+
+extern "C" void init_ili9341_board(GDisplay *g)
+{
+
+}
+
+
+extern "C" void post_init_ili9341_board(GDisplay *g)
+{
+
+}
+
+extern "C" void setpin_ili9341_reset(GDisplay *g, bool_t state)
+{
+
+}
+
+
+extern "C" void set_ili9341_backlight(GDisplay *g, uint8_t percent)
+{
+
+}
+
+
+extern "C" void acquire_ili9341_bus(GDisplay *g);
+extern "C" void release_ili9341_bus(GDisplay *g);
+extern "C" void write_ili9341_index(GDisplay *g, uint16_t index);
+extern "C" void write_ili9341_data(GDisplay *g, uint16_t data);
+extern "C" void ili9341_setreadmode(GDisplay *g);
+extern "C" void ili9341_setwritemode(GDisplay *g);
+extern "C" uint16_t ili9341_read_data(GDisplay *g);
